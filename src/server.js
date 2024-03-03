2503dea0270949f585f6ec38b6538daa1f160bc2
Vlad Belousov <belousov7vlad@gmail.com>
2024-03-03 08:44:14 +0300

    t

--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ import path from 'path';
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("./dist"));
+app.use(express.static("../dist"));
 app.use("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 app.listen(PORT, function () {
