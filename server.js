1dc414f078cc79c980b1b7efd4ee9aa30d444fa9
Vlad Belousov <belousov7vlad@gmail.com>
2024-03-03 08:30:09 +0300

    chore: server test

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("./dist"));
+app.use("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
+});
