7a66bc2abde2c9ea840ee126d152a4fc56c4166c
Vlad Belousov <belousov7vlad@gmail.com>
2024-03-03 08:39:14 +0300

    t

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import { defineConfig } from "vite";
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-global.__dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-  root: resolve(global.__dirname, './src'),
+  root: resolve(__dirname, './src'),
   build: {
-    outDir: resolve(global.__dirname, 'dist'),
+    outDir: resolve(__dirname, './dist'),
   },
   plugins: [handlebars()],
 });
