898e99949c7ef4ed1463968f30489e7ae71034a1
Vlad Belousov <belousov7vlad@gmail.com>
2024-03-13 14:54:48 +0300

    feat: regex added

---
+++ src/utils/constans/regex.ts
@@ -0,0 +1,6 @@
+export const regex_name = /^([А-ЯЁA-Z][а-яёa-z-]*)$/;
+export const regex_login = /^[a-zA-Z_-]{3,20}(?=\D*\d)\D*$/;
+export const regex_email = /^[a-zA-Z0-9_-]+@[a-zA-Z]+\.[a-zA-Z]+$/;
+export const regex_password = /^(?=.*[A-Z])(?=.*\d).{8,40}$/;
+export const regex_phone = /^\+?\d{10,15}$/;
+export const regex_message = /^.*$/;
