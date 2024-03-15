ade34574780b7b75ce4ff1c65a0bf7036fcbbb12
Vlad Belousov <belousov7vlad@gmail.com>
2024-03-15 18:14:00 +0300

    chore: temp data moved

--- src/pages/chat-page/index.ts
+++ src/pages/chat-page/index.ts
@@ -1,19 +1,8 @@
 import Handlebars from 'handlebars';
 import './chat-page.scss';
 export { default as ChatPage } from './chat-page.hbs?raw';
-import opossum from '../../assets/images/opossum_1.png';
-import badger from '../../assets/images/badger_1.png';
-import raccoon from '../../assets/images/enot_1.jpg';
+import {chatData} from '../../data/chatData'
 
 Handlebars.registerHelper('chat-page-list', () => {
-  return [
-    { name: 'Опоссум', message: 'Изображение', unread: '2', avatar: opossum, date: 'Вт' },
-    { name: 'Енот', message: 'Go на свалку!', avatar: raccoon, date: 'Чт' },
-    {
-      name: 'Барсук',
-      message: 'А у кого ключи от сарая?',
-      unread: '4',
-      avatar: badger,
-    },
-  ];
+  return chatData;
 });
