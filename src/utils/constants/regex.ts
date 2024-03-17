15f0b01aae1401f3ce7437fc40a57db6d57a0427
Vlad Belousov <belousov7vlad@gmail.com>
2024-03-17 19:20:05 +0300

    chore: validation utilit

--- src/utils/constants/regex.ts
+++ src/utils/constants/regex.ts
@@ -1,5 +1,5 @@
-export const regex_name = /^([А-ЯЁA-Z][а-яёa-z-]*)$/;
-export const regex_login = /^[a-zA-Z_-]{3,20}(?=\D*\d)\D*$/;
+export const regex_name = /^([А-ЯA-Z][а-яa-z-]*)$/; 
+export const regex_login = /^[a-zA-Z_-]{3,20}\D$/;
 export const regex_email = /^[a-zA-Z0-9_-]+@[a-zA-Z]+\.[a-zA-Z]+$/;
 export const regex_password = /^(?=.*[A-Z])(?=.*\d).{8,40}$/;
 export const regex_phone = /^\+?\d{10,15}$/;
---
+++ src/utils/validation/index.ts
@@ -0,0 +1,23 @@
+import { regex_email, regex_login, regex_name, regex_password, regex_phone } from "utils/constants/regex";
+
+export const validation = (type: string, value: string): boolean => {
+    console.log([value, type, regex_login])
+  switch (type) {
+    case 'login':
+      return regex_login.test(value);
+    case 'password':
+      return regex_password.test(value);
+    case 'email':
+      return regex_email.test(value);
+    case 'name':
+      return regex_name.test(value);
+    case 'phone':
+      return regex_phone.test(value);
+    default:
+      return false; 
+  }
+};
+
+export const doesPasswordMatch = (password: string, repeatPassword: string): boolean => {
+  return password === repeatPassword;
+};
