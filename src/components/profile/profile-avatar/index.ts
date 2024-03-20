780cf9cbd0d4c5cc09b217132d845100a238ef96
Vlad Belousov <belousov7vlad@gmail.com>
2024-03-20 15:11:19 +0300

    t

--- src/components/profile/profile-avatar/index.ts
+++ src/components/profile/profile-avatar/index.ts
@@ -3,7 +3,7 @@ import ProfileAvatarBlock from './avatar.hbs?raw';
 import Block from 'utils/block/Block';
 export default class ProfileAvatar extends Block {
   constructor(props: Record<string, unknown>) {
-    super('form', props);
+    super('div', props);
   }
   render() {
     return this.compile(ProfileAvatarBlock, this.props);
